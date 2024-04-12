b193a3b18430eed2ea1bf1a4c392683f668f2fb2
tchristo <tim.christophe@hotmail.com>
2024-04-12 15:36:37 +0800

    setup of venv and autopy library

---
+++ README.txt
@@ -0,0 +1,4 @@
+Bot to play Idle Planet Miner on BlueStacks Android Emulator.
+Written in Python 3.12.1
+
+This is my first attempt building full programs in Python.
--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+import autopy
+
+def hello_world():
+    autopy.alert.alert("Hello, world")
+
+def move_mouse():
+    autopy.mouse.smooth_move(1, 1)
+
+def type_words():
+    autopy.key.type_string("Hello, world!", wpm=100)
+
+def type_letters():
+    autopy.key.tap(autopy.key.Code.TAB, [autopy.key.Modifier.META])
+    autopy.key.tap("w", [autopy.key.Modifier.META])
+
 if __name__ == '__main__':
-    print('write here')
+    #hello_world()
+    #move_mouse()
+    #type_words()
+    type_letters()

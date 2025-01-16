6bcfc5c3fe18946ef62311fcdb39f6805df7702f
tchri001 <tim.christophe@hotmail.com>
2025-01-16 22:10:17 +0800

    remove old code

--- main.py
+++ main.py
@@ -8,36 +8,10 @@ from overlay import OverlayWindow
 from utils import Utils
 import time
 
-class TransparentOverlay(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.setGeometry(0, 0, QApplication.primaryScreen().size().width(),
-                         QApplication.primaryScreen().size().height())
-        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.Tool)
-        self.setAttribute(Qt.WA_TranslucentBackground)
-        self.setAttribute(Qt.WA_TransparentForMouseEvents)
-        self.show()
-
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.setRenderHint(QPainter.Antialiasing)
-
-        # Draw the rectangle
-        rect = QRect(500, 300, 800, 600)
-        painter.setPen(QColor(0, 255, 0, 255))
-        painter.setBrush(Qt.NoBrush)
-        painter.drawRect(rect)
-        painter.end()
-
-
 def background_logic():
     while True:
         print("Background logic is running!")
         time.sleep(1)
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -50,8 +24,8 @@ if __name__ == "__main__":
     exit_program.start()
 
     # Run background logic in a separate thread
-    #thread = threading.Thread(target=background_logic, daemon=True)q
-    #thread.start()
+    thread = threading.Thread(target=background_logic, daemon=True)
+    thread.start()
 
     # Add some rectangles with different coordinates and sizes
     overlay.add_rectangle(100, 100, 200, 150)  # Rectangle 1
